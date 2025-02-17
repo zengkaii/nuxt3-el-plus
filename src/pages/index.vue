60fc4abf7f3c3b88f8f6089f6b18d27a14e52dd8
zengkai <1965599425@qq.com>
2025-02-17 17:19:57 +0800

    fix

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
     <h1>首页</h1>
-    <el-button type="primary" @click="showPage = 'page1'">page1</el-button>
-    <el-button type="primary" @click="showPage = 'page2'">page2</el-button>
-    <Page1 v-if="showPage === 'page1'" />
-    <Page2 v-if="showPage === 'page2'" />
   </div>
 </template>
-<script setup lang="ts">
-import Page1 from './page1.vue'
-import Page2 from './page2.vue'
-const showPage = ref('page1')
-</script>
+<script setup lang="ts"></script>
