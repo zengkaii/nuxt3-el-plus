018859bde51924ef6683ae6a7166f50d942d1a6d
zengkai <1965599425@qq.com>
2025-02-17 17:28:28 +0800

    fix

--- src/pages/dotlottie.vue
+++ src/pages/dotlottie.vue
@@ -1,13 +1,6 @@
 <template>
   <ClientOnly>
-    <lottie-player
-      v-if="showLottie"
-      autoplay
-      loop
-      style="width: 400px"
-      src="https://lottie.host/844ba406-b54f-4eff-b366-c1b38f9fc322/pYBf9Lz5LX.json"
-      speed="1"
-    />
+    <lottie-player v-if="showLottie" autoplay loop style="width: 400px" src="/loading.json" speed="1" />
   </ClientOnly>
 </template>
 
